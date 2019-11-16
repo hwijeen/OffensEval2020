df4883be74fe9afcecc049ca8645bcafb115c495
hwijeen <hwijeen@gmail.com>
2019-11-15 23:41:49 -0500

    trainer

--- trainer.py
+++ trainer.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 
 
-# In general, early stopping with dev set is needed
+# TODO: early stopping
+# TODO: running avg
 class Trainer():
     def __init__(self, model, train_iter, val_iter, test_iter, optimizer,
                  record_every=100, exp_name=None):
@@ -18,7 +19,7 @@ class Trainer():
 
     def _compute_loss(self, batch):
         logits = self.model(*batch.text)
-        loss = self.criterion(logits, batch.tgt).sum() / len(batch)
+        loss = self.criterion(logits, batch.label)
         return loss
 
     def _compute_entire_loss(self, data_iter):
@@ -27,48 +28,48 @@ class Trainer():
             loss = 0
             num_ex = 0
             for batch in data_iter:
-                loss += self._compute_loss(batch) * len(batch) # silly
+                loss += self._compute_loss(batch) * len(batch)
                 num_ex += len(batch)
             loss = loss / num_ex
         data_iter.repeat = True
         return loss
 
     def train(self, train_step):
+        self.model.train()
         for step, batch in enumerate(self.train_iter, 1):
-
             loss = self._compute_loss(batch)
 
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-            if hasattr(self.model.encoder, 'lstm'):
-                nn.utils.clip_grad_norm_(self.model.parameters(), 5)
-
 
             if step % self.record_every == 0:
-                test_loss = self._compute_entire_loss(self.test_iter)
-                train_acc = self.evaluate(self.train_iter)
-                test_acc = self.evaluate(self.test_iter)
-                self.writer.add_scalar('Loss/train', loss.item(), step)
-                self.writer.add_scalar('Loss/test', test_loss.item(), step)
-                self.writer.add_scalar('Accuracy/train', train_acc, step)
-                self.writer.add_scalar('Accuracy/test', test_acc, step)
+                val_loss = self._compute_entire_loss(self.val_iter)
+                self.record(loss, val_loss)
 
             if step == train_step:
                 self.writer.close()
                 break
 
-    def evaluate(self, data_iter):
-        self.model.eval()
-        data_iter.repeat = False
-        predictions = []
-        truth = []
-        with torch.no_grad():
-            for batch in data_iter:
-                pred = self.model.inference(*batch.sent)
-                predictions += pred.tolist()
-                truth += batch.tgt.tolist()
-        acc = calc_accuracy(predictions, truth)
-        data_iter.repeat = True
-        self.model.train()
-        return acc
+    def record(self, loss, val_loss, step, train_acc=None, val_acc=None):
+        self.writer.add_scalar('Loss/train', loss.item(), step)
+        self.writer.add_scalar('Loss/val', val_loss.item(), step)
+        if train_acc is not None:
+            self.writer.add_scalar('Accuracy/train', train_acc, step)
+        if val_acc is not None:
+            self.writer.add_scalar('Accuracy/val', val_acc, step)
+
+    #def evaluate(self, data_iter):
+    #    self.model.eval()
+    #    data_iter.repeat = False
+    #    predictions = []
+    #    truth = []
+    #    with torch.no_grad():
+    #        for batch in data_iter:
+    #            pred = self.model.inference(*batch.sent)
+    #            predictions += pred.tolist()
+    #            truth += batch.tgt.tolist()
+    #    acc = calc_accuracy(predictions, truth)
+    #    data_iter.repeat = True
+    #    self.model.train()
+    #    return acc
