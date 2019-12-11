bf3ddfc6391685a8aa0c007d16788b3fd510da8b
hwijeen <hwijeen@gmail.com>
2019-12-11 08:52:58 -0500

    apply weight decay

--- optimizer.py
+++ optimizer.py
@@ -2,18 +2,15 @@ from transformers import AdamW, WarmupLinearSchedule
 
 
 # TODO: training strategy
-def build_optimizer_scheduler(model, lr, eps, warmup, train_step):
-    # TODO: below is suggested in McCormick's blog
-    #param_optimizer = list(model.named_parameters())
-    #no_decay = ['bias', 'gamma', 'beta']
-    #optimizer_grouped_parameters = [
-    #    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-    #     'weight_decay_rate': 0.01},
-    #    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-    #     'weight_decay_rate': 0.0}
-    #]
-    #optimizer = AdamW(optimizer_grouped_parameters, lr, eps, correct_bias=False)
-    optimizer = AdamW(model.parameters(), lr, eps, correct_bias=False)
+def build_optimizer_scheduler(model, lr, eps, warmup, weight_decay, train_step):
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters()\
+                    if not any(nd in n for nd in no_decay)], 'weight_decay': weight_decay},
+        {'params': [p for n, p in model.named_parameters()\
+                    if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr, eps, correct_bias=False)
     scheduler = WarmupLinearSchedule(optimizer, warmup, train_step)
     return optimizer, scheduler
 
