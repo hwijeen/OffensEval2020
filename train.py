bfca07159f962eada4f5df5d8e8df327921791cf
jiminsun <jiminxsun@gmail.com>
2019-12-22 14:09:20 -0500

    fix

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ def parse_args():
     model = parser.add_argument_group('Model options')
     model.add_argument('--model', choices=['bert', 'roberta', 'xlm', 'xlnet'], default='bert')
     model.add_argument('--time_pooling', choices=['cls', 'avg', 'max'], nargs='+', default='avg')
-    model.add_argument('--layer_pooling', choices=['avg', 'weighted_avg', 'concat'], nargs='+', default='avg')
+    model.add_argument('--layer_pooling', choices=['avg', 'weighted_avg', 'cat'], nargs='+', default='cat')
     model.add_argument('--layer', type=int, choices=range(1, 13), nargs='+', default=12)
     model.add_argument('--attention_probs_dropout_prob', type=float, default=0.3)
     model.add_argument('--hidden_dropout_prob', type=float, default=0.1)
@@ -82,7 +82,7 @@ def generate_exp_name(args):
     layer = f"layer_{'_'.join(map(str, args.layer))}"
     lr = f'lr_{args.lr}'
     task = f'task_{args.task}'
-    exp_name = '_'.join([model, time_pooling, lr, task, args.note])
+    exp_name = '_'.join([model, time_pooling, lr, task, layer_pooling, layer, args.note])
     return exp_name
 
 # TODO: save args for reproducible exp
